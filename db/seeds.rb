4e3a5fb2cca862100db21a2d4c12068cef7c2f9c
Habib Payenda <habiburahmanpayenda@gmail.com>
2023-01-19 19:12:53 +0430

    Generates messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+5.times do |i|
+  Message.create!(
+    message: "hello #{i}"
+  )
+end
